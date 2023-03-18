2f231dc9330a9e3140c6d26c4ba8e07460401da2
Jimmy Li <jmyli562@gmail.com>
2023-03-18 12:59:04 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+function createFareMultiplier(fareMultiplier){
+    return function(fare){ return fare * fareMultiplier};
+}
+
+function fareDoubler(fare){
+    const doubleOrNothing = createFareMultiplier(2);
+    return doubleOrNothing(fare);
+}
+
+function fareTripler(fare){
+    const tripleOrNothing = createFareMultiplier(3);
+    return tripleOrNothing(fare);
+}
+
+function selectDifferentDrivers(drivers, choice){
+    let newDrivers = [];
+
+    if(choice.name === "returnFirstTwoDrivers"){
+
+        newDrivers = returnFirstTwoDrivers(drivers);
+
+    }else if(choice.name === "returnLastTwoDrivers"){
+
+        newDrivers = returnLastTwoDrivers(drivers);
+    }
+
+    return newDrivers;
+}
+
+/*
+const fareQuintupler = createFareMultiplier(5);
+console.log(fareQuintupler(5));
+*/
+const selectingDrivers = [];
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+selectingDrivers.push(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+
+//console.log(selectingDrivers[0].name);
